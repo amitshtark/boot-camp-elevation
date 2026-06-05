34e1a857bf4a314d9a30471e4e5a67fa4991fd60
Amit Shtark <amit.shtark@gmail.com>
2026-06-05 20:41:52 +0300

    added data structures assignment

---
+++ week 2/data structures/exercise2.js
@@ -0,0 +1,45 @@
+class UniqueArray {
+    constructor() {
+        this.uniqueObj = {};
+        this.uniqueArr = [];
+        this.uniqueLength = 0;
+    }
+
+    showAll(){
+        console.log(this.uniqueArr);
+    }
+
+    add(item) {
+        if(!this.exists(item))
+        {
+            this.uniqueObj[JSON.stringify(item)] = this.uniqueLength;
+            this.uniqueArr[this.uniqueLength] = item;
+            this.uniqueLength++;
+        }
+    }
+
+    exists(item) {
+        if(this.uniqueObj[JSON.stringify(item)] !== undefined)
+            return true;
+        return false;
+    }
+    get(index){
+        if(index >= 0 && index < this.uniqueLength)
+        {
+            return this.uniqueArr[index];
+        }
+        else
+            return -1;
+    }
+}
+
+
+const uniqueStuff = new UniqueArray()
+uniqueStuff.add("toy")
+uniqueStuff.showAll() //prints ["toy"]
+uniqueStuff.add("toy")
+uniqueStuff.showAll() //prints ["toy"]
+uniqueStuff.exists("toy") //returns true
+uniqueStuff.add("poster")
+uniqueStuff.add("hydrogen")
+console.log(uniqueStuff.get(2)) //prints "hydrogen"
